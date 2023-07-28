3fdbf1041704f35adf6058f6d53d3629e8156043
Jaydip1510 <122079821+Jaydip1510@users.noreply.github.com>
2023-07-28 09:46:26 +0530

    delete

--- arrow/arith.js
+++
@@ -1,5 +0,0 @@
-const a = require('./arrow')
-console.log("addition is"+a.add(25,35));
-console.log("minus is"+a.subtract(85,15));
-console.log("multiply is"+a.multiply(15,25));
-console.log("divsion is"+a.divide(415,7));
