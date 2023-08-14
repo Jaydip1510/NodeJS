2d670ade77ba6e8f9bafeeb89cd7469e8bc0afa9
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-08-14 17:51:29 +0530

    changes

--- mgdatabase/src/index.js
+++ mgdatabase/src/index.js
@@ -3,17 +3,18 @@ const app = express();
 const body = require("body-parser");
 const path = require("path");
 const multer = require("multer");
+const fs = require("fs");
 let imgfilename = '';
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-     return cb(null, "./upload/");
+        return cb(null, "./upload/");
     },
     filename: function (req, file, cb) {
-        imgfilename = Date.now()+file.originalname;
-       return cb(null, imgfilename);
+        imgfilename = Date.now() + file.originalname;
+        return cb(null, imgfilename);
     }
 });
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 // create folder
 // const upload = multer({dest:'./upload'})
@@ -21,6 +22,7 @@ const upload = multer({storage: storage});
 const mainpath = path.join(__dirname, "../public");
 app.use(express.static(mainpath));
 app.use(express.static('upload'));
+// const imgmainpath = path.join(__dirname, "../upload");
 app.set("view engine", "ejs");
 const bodyparse = body.urlencoded({ extended: false });
 const mongo = require('mongodb');
@@ -39,19 +41,19 @@ async function getdata() {
         const db = client.db('userinfo')
         const collection = db.collection('userdata');
         const userdata = await collection.find({}).toArray();
-        console.log(userdata);
-
-
         // insert new records
-        app.get("/mydb", (req, res) => {
-            udata ='';
+        app.get("/mydb", async (req, res) => {
+            console.log("MYDB Request Called");
+            udata = '';
+            var userdata1 = await collection.find({}).toArray();
             res.render("index", {
-                data: userdata,
+                data: userdata1,
                 udata: udata
             });
         });
         app.post('/savedata', upload.single('img'), async (req, res) => {
-            id = req.body.id;
+            console.log("Method SaveData Success");
+            let id = req.body.id;
             console.log(imgfilename);
             if (id != '') {
                 udata = '';
@@ -72,41 +74,55 @@ async function getdata() {
                         age: req.body.age,
                         email: req.body.email,
                         address: req.body.address,
-                        img : imgfilename
+                        img: imgfilename
                     }
                 })
             } else {
                 var data = {
-                    id:(userdata.length + 1).toString(),
+                    id: (userdata.length + 1).toString(),
                     name: req.body.name,
                     age: req.body.age,
                     email: req.body.email,
                     address: req.body.address,
-                    img : imgfilename
+                    img: imgfilename
                 }
-            userdata.push(data);
+                userdata.push(data);
 
                 let result = await collection.insertOne(data);
                 console.log(result);
-                    
+
             }
             res.redirect('mydb');
         });
 
         app.get('/del/:id', async (req, res) => {
-            let id = req.params.id;
-            console.log(id);
 
-            let del = await collection.deleteOne({ id: id });
-            let userdata1 = await collection.find({}).toArray();
-            udata = ''
-            res.render("index", {
-                data: userdata1,
-                udata: udata
-            });
+            try {
+                console.log("Method Delete Success");
+                let id = req.params.id;
+                let images = userdata.find((i) => {
+                    return i.id == id;
+                });
+                if (typeof images == "undefined") {
+                    console.log("No images found");
+                } else {
+                    let imgname = "upload/" + images.img
+                    fs.unlink(imgname, () => {
+                    })
+    
+                }
+                await collection.deleteOne({ id: id });
+                res.redirect('/mydb');
+                 
+
+            } catch (e) {
+                console.log(e)
+            }
 
         });
         app.get('/edit/:id', async (req, res) => {
+            console.log("Method Edit Success");
+            userdata = await collection.find({}).toArray();
             let id = req.params.id;
             udata = userdata.find((i) => {
                 return i.id == id;
