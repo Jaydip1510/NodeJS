f8ba54bfb6b3a7f776fb5005a45d09806ebe1b59
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-08-22 09:26:28 +0530

    changes

---
+++ movieproject/controllers/controller.js
@@ -0,0 +1,35 @@
+
+const model = require('../models/usermodel');
+const fs = require('fs');
+let user = '';
+let iname = '';
+const data = async (req, res) => {
+    let user = await model.find(); 
+    console.log(user);
+        res.render('index', {
+        data: user,
+        user: user
+    });  
+}
+const deldata = async (req, res) => {
+     let user = await model.findOne({_id: req.params.id});
+     let img = "uploads/" + user.pimage;
+    fs.unlink(img, () => {
+        console.log("delete");
+    });
+
+    await model.findByIdAndDelete({_id: req.params.id});
+    res.redirect('/crud');
+}
+
+const aditdata = async (req, res) => {
+    let user = await model.findOne({_id: req.params.id});
+    res.render('index', {
+        data: user,
+        user: user
+    });
+
+};
+module.exports = {
+    data, deldata, aditdata
+}
---
+++ movieproject/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.set('view engine', 'ejs');
+async function main() {
+   try {
+      const routs = require('./routes/user');
+      app.use(routs);
+
+   } catch (e) {
+       console.error(e);
+   }
+}
+main()
+app.listen(8001, "127.0.0.1", () => {
+   console.log("listen port 8001");
+})
