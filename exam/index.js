5d63a3822c4b222445193ecfc842f0f7ad58030b
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-08-29 15:33:18 +0530

    changes dirname

--- exam/index.js
+++ exam/index.js
@@ -1,13 +1,15 @@
 const express    = require('express');
-const path = require('path');
 const app        = express();
-const mainpath = path.join(__dirname,"./movieproject");
-app.use(express.static(mainpath));
-app.use(express.static('uploads'));
+
+
+const path      = require('path');
+app.use(express.static(__dirname));
+
 const bodyParser = require('body-parser');
 const cookie     = require('cookie-parser');
 const session    = require('express-session');
 const flash      = require('connect-flash');
+
 const routes     = require('./routes/user');
 
 app.set('view engine','ejs');
@@ -34,14 +36,16 @@ app.get('/logout', (req, res) => {
     res.redirect('/')
 });
 
+
+
+app.get('/register', (req, res) => {
+    res.render('register',{ message:''});
+});
+
 app.get('/', (req, res) => {
     res.render('login',{ message:''});
 });
 
-app.get('/register', (req, res) => {
-    res.render('register',{ message:''});
-})
-
-app.listen(8001,"127.0.0.1",()=>{
-    console.log("listening port in 8001...");
+app.listen(8100,"127.0.0.1",()=>{
+    console.log("listening port in 8100...");
 })
--- exam/routes/user.js
+++ exam/routes/user.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const body = require('body-parser');
-// app = express();
 const router = express.Router();
-// const multer = require('multer');
 const model = require('../models/productmodel');
 const bodyParser = body.urlencoded({ extended: false });
-// app.use(express.static('uploads'));
-// let iname = '';
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         return cb(null, "./uploads/");
-//     },
-//     filename: function (req, file, cb) {
-//         iname = Date.now() + file.originalname;
-//         return cb(null, iname);
-//     }
-//  });
-// const upload = multer({ storage: storage });
-// console.log(iname);
 const {getDashboard,register,checkUserData,getform,categorydata,getproduct,productdetails} = require("../controllers/usercontrollers");
 
 router.get('/dashboard',getDashboard);
