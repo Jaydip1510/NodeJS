7c8464711818e7ef05425c7c5673551173879705
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-09-01 11:21:37 +0530

    changes

--- adminpanel/index.js
+++ adminpanel/index.js
@@ -58,5 +58,5 @@ app.get('/register',(req,res)=>{
     res.render('register');
 })
 app.listen(8004,"127.0.0.1",()=>{
-    console.log("listening port in 8003...");
+    console.log("listening port in 8004...");
 })
