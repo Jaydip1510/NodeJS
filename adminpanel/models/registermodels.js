cf353e62757408fff51f10f0caef24b9c6131c02
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-09-01 17:07:47 +0530

    changes registerpage

--- adminpanel/models/registermodels.js
+++ adminpanel/models/registermodels.js
@@ -1,12 +1,4 @@
 const mongoose = require('mongoose');
-// const maindata =  async ()=>{
-//     const url = "mongodb://127.0.0.1:27017/admin";
-//     await mongoose.connect(url);
-//     console.log('established connection');
-    
-// }
-// maindata();
-
 const registerSchema = new mongoose.Schema({
     id:Number,
     email:{ type:String, required:true, unique:true },
