03789fd817db85fa0be13ba7b53ee951befa2ade
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-09-23 18:54:52 +0530

    flashmsg

--- adminpanel/controllers/subcategory.js
+++ adminpanel/controllers/subcategory.js
@@ -14,36 +14,46 @@ app.use(express.json());
 const subcategorydata = async (req, res) => {
     let allsubcat = await subcatModel.find();
     const name = req.body.name;
-    const id = req.body.cat_id;   
+    const id = req.body.cat_id;
     const checkName = await subcatModel.findOne({ name: name });
+    if (checkName) {
+        req.flash('msg_category', 'Subcategory already exists.');
+        req.flash('msg_class', 'alert-success');
+        res.redirect("/subcategory/alldata");
+    } else{
 
-    const result = {
-        cat_id: id,
-        name: name
+    
+
+        const result = {
+            cat_id: id,
+            name: name
+        }
+        const savedata = new subcatModel(result);
+        await savedata.save();
+
+        allsubcat = await subcatModel.find();
+        req.flash('msg_category', 'data inserted successfully');
+        req.flash('msg_class', 'alert-success');
+        res.redirect("/subcategory/alldata");
     }
-    const savedata = new subcatModel(result);
-    await savedata.save();
-
-    allsubcat = await subcatModel.find();
-    res.redirect("/subcategory/alldata");
-
 }
 
 // update subcategory 
 
-const updatesubcat = async (req,res)=>{
+const updatesubcat = async (req, res) => {
     let getAllCat = await subcatModel.find();
-   console.log(getAllCat);
+    console.log(getAllCat);
     const name = req.body.name;
     const id = req.body.cat_id;
     const subid = req.params.id;
-    const result = await subcatModel.findByIdAndUpdate({_id:subid},{
-        $set:{
-            name:name,
-            cat_id:id
+    const result = await subcatModel.findByIdAndUpdate({ _id: subid }, {
+        $set: {
+            name: name,
+            cat_id: id
         }
     })
-    console.log("Subcat updated");
+    req.flash('msg_category', 'SubCategory updated successfully');
+    req.flash('msg_class', 'alert-success');
     res.redirect("/subcategory/alldata");
 }
 
@@ -51,7 +61,7 @@ const updatesubcat = async (req,res)=>{
 
 const SubCatData = async (req, res) => {
     let catData = await categoryModel.find();
-   // console.log(catData);
+    // console.log(catData);
     const joindata = await subcatModel.find().populate("cat_id");
     res.render('subcat', {
         username: req.cookies.UserName,
@@ -59,7 +69,9 @@ const SubCatData = async (req, res) => {
         catData: catData,
         userimage: req.cookies.image,
         selected: 'subcat',
-        subcatedit:''
+        subcatedit: '',
+        message: req.flash('msg_category'),
+        message_class: req.flash('msg_class'),
     });
 
 }
@@ -69,6 +81,8 @@ const SubCatData = async (req, res) => {
 const subcatdelete = async (req, res) => {
     const id = req.params.id;
     const data = await subcatModel.findByIdAndRemove({ _id: id });
+    req.flash('msg_category', 'data deleted successfully');
+    req.flash('msg_class', 'alert-success');
     res.redirect("/subcategory/alldata");
 }
 
@@ -76,16 +90,17 @@ const subcatedit = async (req, res) => {
     let id = req.params.id;
     let catData = await categoryModel.find();
     let subdata = await subcatModel.find().populate('cat_id');
-    result = await subcatModel.findOne({_id:id});
+    result = await subcatModel.findOne({ _id: id });
     res.render('subcat', {
         username: req.cookies.UserName,
         AllSubCat: subdata,
         catData: catData,
         userimage: req.cookies.image,
         selected: 'subcat',
-        subcatedit:result
+        subcatedit: result,
+        message: ''
     });
-    
+
 
 
 
